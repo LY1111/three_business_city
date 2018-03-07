35e1c8f6c69297d55b14ab050044df4ab65e0f15
302638220@qq.com <Glp930615>
2018-03-08 00:18:35 +0800

    2018年3月8日  我的优惠券、个人资料、添加地址

---
+++ app/src/main/java/com/tuoyi/threebusinesscity/activity/MyCouponActivity.java
@@ -0,0 +1,15 @@
+package com.tuoyi.threebusinesscity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tuoyi.threebusinesscity.R;
+
+public class MyCouponActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_coupon);
+    }
+}
